158680e2fd9f163f2ee2f0df3cf8f64b078ebf3b
Junaid Ali <junaidali.yahya@gmail.com>
2019-08-25 16:41:45 +0100

    feat: Added `install.sh` script
    Signed-off-by: Junaid Ali <junaidali.yahya@gmail.com>

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Personalized settings for terminal
 ## Installation
 ```
 $ git clone https://github.com/junaid-ali/dotfiles .dotfiles
-$ echo "source ~/.dotfiles/linux/bashrc/bashrc ~/.dotfiles/git-prompt.sh" >> ~/.bashrc
+$ cd ~/.dotfiles
+$ ./install.sh
 ```
---
+++ install.sh
@@ -0,0 +1,10 @@
+insert_ifnotfound() {
+  grep "${1}" "${2}" > /dev/null 2>&1
+  [[ $? != 0 ]] && echo -e "${1}" >> "${2}"
+}
+
+insert_ifnotfound "source ~/.dotfiles/linux/bashrc/bashrc" ~/.bashrc
+insert_ifnotfound "source ~/.dotfiles/git-prompt.sh" ~/.bashrc
+
+cp linux/vim/colors/* ~/.vim/colors/
+insert_ifnotfound "$(cat linux/vim/vimrc)" ~/.vimrc
