d5026b55b438f133ed5e6b004bb823ed2726280e
Junaid Ali <junaidali.yahya@gmail.com>
2020-01-15 11:58:32 +0000

    fix: Improving installer script
    Signed-off-by: Junaid Ali <junaidali.yahya@gmail.com>

--- install.sh
+++ install.sh
@@ -3,9 +3,17 @@ insert_ifnotfound() {
   [[ $? != 0 ]] && echo -e "${1}" >> "${2}"
 }
 
+# Changing the PWD to this script directory
+cd "$( dirname "${BASH_SOURCE[0]}")"
+mkdir -p ~/.dotfiles
+cp -rf * ~/.dotfiles/
+
 insert_ifnotfound "source ~/.dotfiles/linux/bashrc/bashrc" ~/.bashrc
 insert_ifnotfound "source ~/.dotfiles/git-prompt.sh" ~/.bashrc
 
 mkdir -p ~/.vim/colors
 cp linux/vim/colors/* ~/.vim/colors/
 insert_ifnotfound "$(cat linux/vim/vimrc)" ~/.vimrc
+
+echo -e "Please run the following command for the changes to take effect in your current shell:
+$ source ~/.bashrc"
